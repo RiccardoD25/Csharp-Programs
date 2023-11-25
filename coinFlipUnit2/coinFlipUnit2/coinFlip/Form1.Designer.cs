2569018d3840ff22392bac76bb39b8b31c755c4b
Riccardo De Simini <127334418+RiccardoD25@users.noreply.github.com>
2023-11-25 16:33:00 -0500

    Add files via upload

---
+++ coinFlipUnit2/coinFlipUnit2/coinFlip/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace coinFlip
+{
+    partial class HeadsOrTailsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CoinHeadsPictureBox = new System.Windows.Forms.PictureBox();
+            this.CoinTailsPictureBox = new System.Windows.Forms.PictureBox();
+            this.ShowHeadsButton = new System.Windows.Forms.Button();
+            this.ShowTailsButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CoinHeadsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CoinTailsPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CoinHeadsPictureBox
+            // 
+            this.CoinHeadsPictureBox.BackColor = System.Drawing.Color.Gray;
+            this.CoinHeadsPictureBox.BackgroundImage = global::coinFlip.Properties.Resources.Heads1;
+            this.CoinHeadsPictureBox.Location = new System.Drawing.Point(130, 12);
+            this.CoinHeadsPictureBox.Name = "CoinHeadsPictureBox";
+            this.CoinHeadsPictureBox.Size = new System.Drawing.Size(517, 500);
+            this.CoinHeadsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.CoinHeadsPictureBox.TabIndex = 0;
+            this.CoinHeadsPictureBox.TabStop = false;
+            // 
+            // CoinTailsPictureBox
+            // 
+            this.CoinTailsPictureBox.BackColor = System.Drawing.Color.Gray;
+            this.CoinTailsPictureBox.BackgroundImage = global::coinFlip.Properties.Resources.Tails1;
+            this.CoinTailsPictureBox.Location = new System.Drawing.Point(689, 12);
+            this.CoinTailsPictureBox.Name = "CoinTailsPictureBox";
+            this.CoinTailsPictureBox.Size = new System.Drawing.Size(517, 500);
+            this.CoinTailsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.CoinTailsPictureBox.TabIndex = 1;
+            this.CoinTailsPictureBox.TabStop = false;
+            this.CoinTailsPictureBox.Visible = false;
+            // 
+            // ShowHeadsButton
+            // 
+            this.ShowHeadsButton.AccessibleDescription = "";
+            this.ShowHeadsButton.AccessibleName = "";
+            this.ShowHeadsButton.BackColor = System.Drawing.Color.LawnGreen;
+            this.ShowHeadsButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ShowHeadsButton.Location = new System.Drawing.Point(510, 673);
+            this.ShowHeadsButton.Name = "ShowHeadsButton";
+            this.ShowHeadsButton.Size = new System.Drawing.Size(192, 93);
+            this.ShowHeadsButton.TabIndex = 2;
+            this.ShowHeadsButton.Text = "Show Heads";
+            this.ShowHeadsButton.UseVisualStyleBackColor = false;
+            this.ShowHeadsButton.Click += new System.EventHandler(this.ShowHeadsButton_Click);
+            // 
+            // ShowTailsButton
+            // 
+            this.ShowTailsButton.BackColor = System.Drawing.Color.Yellow;
+            this.ShowTailsButton.Location = new System.Drawing.Point(752, 673);
+            this.ShowTailsButton.Name = "ShowTailsButton";
+            this.ShowTailsButton.Size = new System.Drawing.Size(192, 93);
+            this.ShowTailsButton.TabIndex = 3;
+            this.ShowTailsButton.Text = "Show Tails";
+            this.ShowTailsButton.UseVisualStyleBackColor = false;
+            this.ShowTailsButton.Click += new System.EventHandler(this.ShowTailsButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.Red;
+            this.ExitButton.Location = new System.Drawing.Point(1014, 673);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(192, 93);
+            this.ExitButton.TabIndex = 4;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // HeadsOrTailsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1482, 803);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ShowTailsButton);
+            this.Controls.Add(this.ShowHeadsButton);
+            this.Controls.Add(this.CoinTailsPictureBox);
+            this.Controls.Add(this.CoinHeadsPictureBox);
+            this.Name = "HeadsOrTailsForm";
+            this.Text = "COIN FLIP";
+            ((System.ComponentModel.ISupportInitialize)(this.CoinHeadsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CoinTailsPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox CoinHeadsPictureBox;
+        private System.Windows.Forms.PictureBox CoinTailsPictureBox;
+        private System.Windows.Forms.Button ShowHeadsButton;
+        private System.Windows.Forms.Button ShowTailsButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ coinFlipUnit2/coinFlipUnit2/coinFlip/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace coinFlip
+{
+    public partial class HeadsOrTailsForm : Form
+    {
+        public HeadsOrTailsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowHeadsButton_Click(object sender, EventArgs e)
+        {
+            CoinHeadsPictureBox.Visible = true;
+            CoinTailsPictureBox.Visible = false;
+        }
+
+        private void ShowTailsButton_Click(object sender, EventArgs e)
+        {
+            CoinHeadsPictureBox.Visible=false;
+            CoinTailsPictureBox.Visible=true; 
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
